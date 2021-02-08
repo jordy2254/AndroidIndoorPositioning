e99c7aac623bb229bfaddcae212b5f90b1cbf941
jordy2254 <jordanhellier@googlemail.com>
2021-02-08 14:17:04 +0000

    improve layout

---
+++ app/src/main/java/com/jordan/renderengine/graphics/Drawable.java
@@ -0,0 +1,12 @@
+package com.jordan.renderengine.graphics;
+
+import android.graphics.Canvas;
+
+import com.jordan.renderengine.Screen;
+import com.jordan.renderengine.data.Point2d;
+
+public interface Drawable {
+
+    void draw(Canvas canvas, Point2d offset, double scale);
+
+}
