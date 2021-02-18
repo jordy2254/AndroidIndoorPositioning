7652e427fefb7a7f5e08cb9e06baf88c44c06798
jordy2254 <jordanhellier@googlemail.com>
2021-02-18 15:46:51 +0000

    break bluetooth scanner out into singleton service, rather than static functons

---
+++ app/src/main/java/com/jordan/ips/model/locationTracking/LocationService.java
@@ -0,0 +1,27 @@
+package com.jordan.ips.model.locationTracking;
+
+import com.jordan.ips.model.data.map.persisted.Sensor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocationService {
+
+    private static LocationService INSTANCE = null;
+
+    public double getDistanceFromSensor(String sensorId){
+        return 0D;
+    }
+
+    public List<String> getSensorIdsInRange(){
+        return new ArrayList<>();
+    }
+
+
+    public synchronized static LocationService getINSTANCE() {
+        if (INSTANCE == null) {
+            INSTANCE = new LocationService();
+        }
+        return INSTANCE;
+    }
+}
